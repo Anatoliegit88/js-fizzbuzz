d263c8fc7b3cc51f166e38ba2579306c916f09c8
Anatoliegit88 <anatolie.coroban88@gmail.com>
2022-10-10 17:52:52 +0200

    update

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
-for (let i = 0; i <= 100; i++) {
+for (let i = 1; i <= 100; i++) {
   let multiplo = "";
-  if (i % 3 === 0) {
+  if (i % 3 === 0 && i % 5 === 0) {
+    multiplo = "FizzBuzz";
+  } else if (i % 3 === 0) {
     multiplo = "Fizz";
-  } else if (i % 5 === 0) {
-    multiplo = "Buzz";
   } else {
-    console.log(i);
+    if (i % 5 === 0) {
+      multiplo = "Buzz";
+    } else {
+      console.log(i);
+    }
   }
   console.log(multiplo);
 }
