ed7576caab956e375518d3e0e107cdf0db096d52
Anatoliegit88 <anatolie.coroban88@gmail.com>
2022-10-11 09:43:43 +0200

    bonus1

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 for (let i = 1; i <= 100; i++) {
   let multiplo = "";
+  const numberitem = document.getElementById("id-number");
+  const item = document.createElement("button");
   if (i % 3 === 0 && i % 5 === 0) {
     multiplo = "FizzBuzz";
   } else if (i % 3 === 0) {
@@ -9,7 +11,12 @@ for (let i = 1; i <= 100; i++) {
       multiplo = "Buzz";
     } else {
       console.log(i);
+      item.innerHTML = `${i}`;
+      numberitem.append(item);
     }
   }
   console.log(multiplo);
+  item.innerHTML = `${multiplo}`;
+  numberitem.append(item);
 }
+// BONUS
